8f3756e9386e0058f8e789d129763b098be15602
Devendra <devendrakandpal07@gmail.com>
2024-04-28 01:30:47 +0530

    committed

--- todolist/frontend/src/App.jsx
+++ todolist/frontend/src/App.jsx
@@ -1,17 +1,24 @@
 import {React} from 'react';
 import {Routes, Route} from 'react-router-dom'
+import Auth from './auth/auth';
+import Signin from './auth/signin';
+import Signup from './auth/signup';
+import Rootlayout from './rootlayout/root'
+import Home from './pages/home' 
 function App() {
 
   return (
     <div>
       <Routes>
       {/* Public Route */}
-        <Route>
-          
+        <Route element={< Auth />}>
+          <Route path='/sigin' element = {< Signin />}/>
+          <Route path='/sigup' element = {< Signup />}/>
+
         </Route>
         {/* Private Route */}
-        <Route>
-          
+        <Route element={< Rootlayout />}>
+          <Route path='/' element={< Home />} />
         </Route>
       </Routes>
     </div>
