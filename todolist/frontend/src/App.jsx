db3e2e3ca8234f41cdb7399d099cb653d7fab16e
Devendra <devendrakandpal07@gmail.com>
2024-04-28 20:48:59 +0530

    committed

--- todolist/frontend/src/App.jsx
+++ todolist/frontend/src/App.jsx
@@ -1,29 +1,17 @@
 import {React} from 'react';
 import {Router ,Routes, Route} from 'react-router-dom';
-import Authz from "./auth/authorisation";
 import Signin from "./auth/forms/signin";
 import Signup from "./auth/forms/signup"; 
-import Rootlayout from './rootlayout/root'
-import Home from './pages/home' 
 function App() {
 
   return (
     <main>
       <Routes>
-
-
-        <Route element = {<Authz/>}>
+       <Route>
       {/* Public Route */}
-          <Route path='/signin' element = {< Signin />}/>
+          <Route path='/' element = {< Signin />}/>
           <Route path='/signup' element = {< Signup />}/>
         </Route>
-
-
-        <Route element={<Rootlayout />}>
-        {/* Private Route */}
-          <Route index element={< Home />} />
-        </Route>
-
 
       </Routes>
     </main>
--- todolist/frontend/src/auth/forms/signin.jsx
+++ todolist/frontend/src/auth/forms/signin.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import './css/signn.css'
 
 const signin = () => {
   return (
-    <div>
-      <button>
-        Signin
-      </button>
+    <div className='main-signin'>
+       <div className="signinimages">
+        <img src="../public/clock4.jpg" alt="" />
+      </div>
+      <div className="signin-form">
+        <div className="signin-formdetails">
+          <h1>Sign in</h1>
+          <label>Email</label><br/>
+          <input type="text" placeholder='Email'/><br /><br />
+          
+          <label>Password</label><br/>
+          <input type="password" placeholder='Password'/><br /><br />
+          <button>Sign in </button>
+        </div>
+      </div>
+     
     </div>
   )
 }
