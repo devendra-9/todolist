316b3c37ba32f4dbeaac56fab0a3e5dc2006f876
Devendra <devendrakandpal07@gmail.com>
2024-05-01 04:14:38 +0530

    committed

--- todolist/frontend/src/pages/datalist.jsx
+++ todolist/frontend/src/pages/datalist.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import './css/detaiil.css'
 
 const datalist = () => {
   return (
-    <div style={{color:'white'}}>datalist</div>
+    <div className='details-main'>
+      <div className="details-tog">
+      <div className="details-input">
+        <input type="text" placeholder='Enter your text here'/>
+        <button>Add To TODO</button>
+      </div>
+      <div className="display-todo">
+      <h1>ALL TODS LIST</h1>
+      </div>
+      </div>
+    </div>
   )
 }
 
